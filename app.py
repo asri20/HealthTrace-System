37949f91094f671c1fd8de202d612b63eca9eafb
asri20 <154069129+asri20@users.noreply.github.com>
2026-04-17 11:12:01 +0700

    Tes ombak

--- app.py
+++ app.py
@@ -85,6 +85,6 @@ def delete(id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.drop_all()  
+        # db.drop_all()  
         db.create_all()
     app.run(host='0.0.0.0', port=80)
